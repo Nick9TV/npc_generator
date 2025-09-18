745fed2d9c54d442c50d0d5916e8dc0230f0d9b9
Mikołaj Hoffmann <e.m4il@op.pl>
2025-09-18 20:17:02 +0200

    Updated config.

--- npc_generation/config.py
+++ npc_generation/config.py
@@ -6,3 +6,9 @@ load_dotenv()
 
 class settings:
     OPEN_API_KEY = os.getenv("OPEN_API_KEY")
+    
+    CHUNK_SIZE = os.getenv("CHUNK_SIZE")
+    
+    CHUNK_OVERLAP = os.getenv("CHUNK_OVERLAP")
+    
+    INDEX_PATH = os.getenv("INDEX_PATH")
