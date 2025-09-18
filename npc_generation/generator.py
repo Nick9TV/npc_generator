af0e93c2a8e21c05934cbc5bb9d3178f9948b807
Mikołaj Hoffmann <e.m4il@op.pl>
2025-09-18 22:17:44 +0200

    Fixed typo in funcion name from roaster -> roster

--- npc_generation/generator.py
+++ npc_generation/generator.py
@@ -88,5 +88,5 @@ def generate_charactre(index: StoryIndex, request: str, ensure_unique: bool=True
     
     return data
 
-def list_roaster() -> List[Dict[str, Any]]:
+def list_roster() -> List[Dict[str, Any]]:
     return _load_roster()
